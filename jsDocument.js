dbd242f778b6e8744e34377b08eee79c9c575149
Erick Silva <106249621+Ericksilvadeoliveira@users.noreply.github.com>
2022-10-07 21:32:53 -0300

    display, numbers

--- jsDocument.js
+++ jsDocument.js
@@ -15,28 +15,41 @@ function multiply(num1, num2) {return res = num1 * num2;};
 //DIVISÃO
 function divide(num1, num2) {if (num1 < num2) {return res = num2 / num1} {return res = num1 / num2;}};
 
-//OPERAÇÃO
-const operate = (num1, num2, operator) => {
-    if (operator === '+') {
-        return add(num1, num2);
-    } if (operator === '-') {
-        return subtract(num1, num2);
-    } if (operator === '*') {
-        return multiply(num1, num2);
-    } {
-        return divide(num1, num2);
-    }
-};
-
 allButtons.forEach(button => {
     button.addEventListener('click', (c) => {
         const value = c.target.dataset.value;
 
-        if (display.innerText.includes('+')) {
-            const split = display.innerText.split('+')
-            operate(split[0], split[1], '+')
+        if (value === '=') {
+            if (display.innerText.includes('+')) {
+                const split = display.innerText.split('+');
+                operate('+', split[0], split [1]);
+            } 
         }
-
         display.innerText += value;
+
+        if (value === 'C') {
+            display.innerText = '';
+        }
     })
 })
+
+const operate = (operator, num1, num2) => {
+    value1 = Number(num1);
+    value2 = Number(num2);
+
+    if (operator === '+') {
+        display.innerText = value1 + value2;
+    }
+    
+    if (operator === '-') {
+        return subtract(num1, num2);
+    }
+    
+    if (operator === '*') {
+        return multiply(num1, num2);
+    }
+    
+    if (operator === '/') {
+        return divide(num1, num2);
+    }
+};
